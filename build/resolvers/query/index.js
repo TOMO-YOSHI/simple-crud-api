9a17263dc3874a7883701b73e7f451556ec70bc4
TOMO-YOSHI <lucky.paddy10@gmail.com>
2022-09-21 22:43:55 -0700

    update readme and fix build bug

--- build/resolvers/query/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.todos = exports.todo = void 0;
-var todo_1 = require("./todo");
-Object.defineProperty(exports, "todo", { enumerable: true, get: function () { return todo_1.todo; } });
-var todos_1 = require("./todos");
-Object.defineProperty(exports, "todos", { enumerable: true, get: function () { return todos_1.todos; } });
